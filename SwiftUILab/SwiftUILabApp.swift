3cd78ee00609180289d4a52137e80037432ee18b
devhaute <devhaute@gmail.com>
2021-10-18 19:37:43 +0900

    기본 환경 설정

---
+++ SwiftUILab/SwiftUILabApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILabApp.swift
+//  SwiftUILab
+//
+//  Created by devhaute on 2021/10/18.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
